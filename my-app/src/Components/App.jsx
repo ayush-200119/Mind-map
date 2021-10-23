f57f9605473b4695a34140e8b29f1ab2a9d22df7
unknown <ayushsalvi19@gmail.com>
2021-10-23 16:05:14 +0530

    Add Clickable Card

--- my-app/src/Components/App.jsx
+++ my-app/src/Components/App.jsx
@@ -6,12 +6,12 @@ function App()
     return (
         <React.StrictMode>
         <div>
-        <Card heading="Topic" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"/>
-        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"/>
-        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"/>
-        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"/>
-        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"/>
-        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do"/>
+        <Card heading="Topic" description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" link="https://www.google.co.in"/>
+        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" link="https://www.google.co.in"/>
+        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" link="https://www.google.co.in"/>
+        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" link="https://www.google.co.in"/>
+        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" link="https://www.google.co.in"/>
+        <Card description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do" link="https://www.google.co.in"/>
     </div>
       </React.StrictMode>);
 }
--- my-app/src/Components/Card.jsx
+++ my-app/src/Components/Card.jsx
@@ -1,19 +1,12 @@
 import React from 'react'
 import { Card } from 'semantic-ui-react'
 
-const CardExampleHeaderCard = (props) => (
-  <Card.Group>
-    <Card>
-      <Card.Content>
-        <Card.Header>{props.heading}</Card.Header>
-        {/* <Card.Meta>Co-Worker</Card.Meta> */}
-        <Card.Description>
-          {props.description}
-        </Card.Description>
-      </Card.Content>
-    </Card>
-    
-  </Card.Group>
+const CardExampleLinkCard = (props) => (
+  <Card
+    href={props.link}
+    header={props.heading}
+    description={props.description}
+  />
 )
 
-export default CardExampleHeaderCard
+export default CardExampleLinkCard
